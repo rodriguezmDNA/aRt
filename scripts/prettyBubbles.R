2c2d456381ab16faa3c215e113929775301a99d2
rodriguezmDNA <joelrome88@gmail.com>
2021-01-04 21:36:55 -0600

    more bubbles

--- scripts/prettyBubbles.R
+++ scripts/prettyBubbles.R
@@ -16,3 +16,28 @@ data %>%
   geom_point(alpha=0.25,show.legend = FALSE) +
   geom_point(alpha=0.25,aes(y=Petal.Length, x=Petal.Width, size=PetArea, color=Species),show.legend = FALSE) +
   theme_void()
+
+
+genData <- function(x) runif(500)
+
+data <- replicate(5,genData())
+colnames(data) <- letters[1:ncol(data)]
+data <- tibble(data,'ids'=seq(1,nrow(data)))
+
+tibble(data) %>% pivot_longer(!ids,cols=c(a,b))
+
+data = anscombe %>%
+  pivot_longer(everything(),
+               names_to = c(".value", "set"),
+               names_pattern = "(.)(.)"
+  )
+
+data %>% mutate(z=x*y,
+                w=sin(x),
+                v=tan(y)) %>%
+  ggplot(aes(x=x, y=y, size=z, color=set)) +
+  geom_point(alpha=0.15,show.legend = FALSE) +
+  geom_point(aes(x=y,y=x,size=w**2),alpha=0.75,show.legend = FALSE) +
+  geom_point(aes(x=w/2,y=v*w,size=1/z**2),alpha=0.45,show.legend = FALSE) +
+  geom_point(aes(x=v,y=w,size=z**2),alpha=0.45,show.legend = FALSE) +
+  theme_void()  
