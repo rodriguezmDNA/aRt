1d5a125b119f430723ccb7f422fad930aec8640c
rodriguezmDNA <joelrome88@gmail.com>
2021-01-11 09:23:51 -0600

    find center of square

--- scripts/tryRotateSquare.R
+++ scripts/tryRotateSquare.R
@@ -103,3 +103,18 @@ polygon(x=rotated[,'x'],
         y=rotated[,'y'])
 polygon(x=sq[,'x']-.9,
         y=sq[,'y'])
+
+##### Center of square
+plot.new()
+
+
+Xor <- 0.5
+Xwi <- 0.3
+Yor <- 0.5
+Yhe <- 0.2
+polygon(c(Xor,Xor,Xor+Xwi,Xor+Xwi),
+        c(Yor,Yor+Yhe,Yor+Yhe,Yor),col='transparent')
+
+p=Xor + Xwi - ((Xor + Xwi-Xor)/2)
+q=Yor+Yhe - ((Yor+Yhe-Yor)/2)
+points(p,q)
