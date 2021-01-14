6d164020825c4397fc99dc5e1fd870ce611d81c7
rodriguezmDNA <joelrome88@gmail.com>
2021-01-13 21:14:15 -0800

    grid art with center of square

--- scripts/centerOfSquare.R
+++ scripts/centerOfSquare.R
@@ -1,13 +1,28 @@
 plot.new()
 
 
-Xor <- 0.5
-Xwi <- 0.3
-Yor <- 0.5
-Yhe <- 0.2
-polygon(c(Xor,Xor,Xor+Xwi,Xor+Xwi),
-        c(Yor,Yor+Yhe,Yor+Yhe,Yor),col='transparent')
 
-p=Xor + Xwi - ((Xor + Xwi-Xor)/2)
-q=Yor+Yhe - ((Yor+Yhe-Yor)/2)
-points(p,q)
+drawSQ <- function( Xor=0.5,Xwi = 0.3,Yor = 0.5,Yhe = 0.2,offX=0,offY=0){
+  
+  p= (Xor + Xwi - ((Xor + Xwi - Xor)/2)) + offX
+  q= (Yor + Yhe - ((Yor + Yhe - Yor)/2)) + offY
+  
+  
+  polygon(c(Xor,Xor,Xor+Xwi,Xor+Xwi)+offX,
+        c(Yor,Yor+Yhe,Yor+Yhe,Yor)+offY,col='transparent')
+  points(p,q)
+  return(c(p,q))
+}
+
+
+plot.new()
+for (i in seq(-1,1,.05)){
+  for (j in seq(-1,1,.05)){
+    drawSQ(Xwi = 0.2+j+i,Yhe = +j+i,offX = i,offY = j)  
+  }
+}
+
+drawSQ(offX = 0.1)
+drawSQ(offX = 0.1,offY = -0.5)
+
+
