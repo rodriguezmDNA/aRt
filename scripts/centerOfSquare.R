10c2b17eefc020ba0d1e32f03ccfdc84d8f407b3
rodriguezmDNA <joelrome88@gmail.com>
2021-01-15 16:14:14 -0800

    fix center point

--- scripts/centerOfSquare.R
+++ scripts/centerOfSquare.R
@@ -1,12 +1,28 @@
+
 plot.new()
 
+Xor = 0.5
+Yor = 0.5
+Yhe = 0.7
+offX = -0.3
+offY = -0.5
+
+polygon(c(Xor,Xor,Xor+Xwi,Xor+Xwi),
+        c(Yor,Yor+Yhe,Yor+Yhe,Yor),col='transparent')
+
+p= (Xor + Xwi) - ( Xwi/2)
+q= (Yor + Yhe) - ( Yhe/2)
+points(p,q)
+
+
+polygon(c(Xor,Xor,Xor+Xwi,Xor+Xwi)+offX,
+        c(Yor,Yor+Yhe,Yor+Yhe,Yor)+offY,col='transparent')
 
 
 drawSQ <- function( Xor=0.5,Xwi = 0.3,Yor = 0.5,Yhe = 0.2,offX=0,offY=0,centerPoint=T){
   
-  p= (Xor + Xwi - ((Xor + Xwi - Xor)/2)) + offX
-  q= (Yor + Yhe - ((Yor + Yhe - Yor)/2)) + offY
-  
+  p= (Xor + Xwi) - ( Xwi/2) + offX
+  q= (Yor + Yhe) - ( Yhe/2) + offY
   
   polygon(c(Xor,Xor,Xor+Xwi,Xor+Xwi)+offX,
         c(Yor,Yor+Yhe,Yor+Yhe,Yor)+offY,col='transparent')
@@ -26,6 +42,9 @@ for (i in seq(-1,1,.05)){
            Yhe = 0.2+j+i,offX = i,offY = j,centerPoint = setpoint)  
   }
 }
+
+
+
 
 
 plot.new()
@@ -51,7 +70,15 @@ for (i in seq(-1,1,.05)){
     he=sample(seq(0.3,1,.01),1)
     xS = sample(seq(1,-1),1)
     yS = sample(seq(1,-1),1)
-    drawSQ(Xwi = wi+j+i,
+    if (!setpoint){
+      setpoint = sample(c(T,F),prob = c(0.8,0.2),size=1)
+      drawSQ(Xwi = wi+j+i,
            Yhe = he+j+i,offX = i+wi*xS,offY = j+he*yS,centerPoint = setpoint)  
+    }
   }
 }
+
+plot.new()
+drawSQ(Xor = 0.5,Yor = 0.5,Xwi = 0.7)
+drawSQ(Xor = 0.5,Yor = 0.5,Yhe = 0.7,offX = -0.3,offY = -0.3)
+drawSQ(Xor = 0.5,Yor = 0.5,Yhe = 0.7,offX = -0.3,offY = -0.3)
