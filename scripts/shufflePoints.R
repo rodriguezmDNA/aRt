42327628bfaa8f99b303674a8d887d36ee1e4630
rodriguezmDNA <joelrome88@gmail.com>
2021-02-02 18:38:36 -0800

    variations on octagon

--- scripts/shufflePoints.R
+++ scripts/shufflePoints.R
@@ -77,7 +77,6 @@ polygon(cuad[,1],cuad[,2])
 
 
 
-
 rDF <- cuad[sample(nrow(cuad)),]
 rDF <- rDF + 0.5
 rDF <- rDF - 0.5
@@ -105,3 +104,39 @@ df2 <- rbind(
   c(-0.4,0.2),
   c(-0.4,0.4),
   c(-0.2,0.4))
+
+
+
+
+octaFunc <- function(Xcenter = 0,Ycenter = 0,wi = 0.4,he = 0.4){
+  oct <- rbind(
+        c(Xcenter+(wi/2),Ycenter+he),
+        c(Xcenter+wi,Ycenter+(he/2)),
+        c(Xcenter+wi,Ycenter-(he/2)),
+        c(Xcenter+(wi/2),Ycenter-he),
+        c(Xcenter-(wi/2),Ycenter-he),
+        c(Xcenter-wi,Ycenter-(he/2)),
+        c(Xcenter-wi,Ycenter+(he/2)),
+        c(Xcenter-(wi/2),Ycenter+he))
+  return (oct)
+  }
+
+
+
+plot(0, 0,
+     xlim=c(-5,17),
+     ylim=c(-5,17),
+     col = "white", xlab = "", ylab = "", axes=F)
+for (p in seq(1,15)){
+  for (q in seq(1,15)) {
+    oct <- octaFunc(p,q)
+    oct <- oct[sample(nrow(oct)),]
+    polygon(oct[,1],oct[,2])
+}}
+
+
+
+
+
+
+polygon(oct[,1],oct[,2])
