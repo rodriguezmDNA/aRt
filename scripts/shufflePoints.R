d6e6cae51bfef8fc9ff20eac4dc87a90bc374572
rodriguezmDNA <joelrome88@gmail.com>
2021-02-09 09:05:02 -0800

    glyphs

--- scripts/shufflePoints.R
+++ scripts/shufflePoints.R
@@ -121,24 +121,43 @@ plot(0, 0,
 for (p in seq(1,15)){
   for (q in seq(1,15)) { 
     
-    choice <- sample(1:4,1)
-    
+    choice <- sample(1:5,1)
+    w = sample(seq(0.3,0.8,0.5),1)
+    h = sample(seq(0.3,0.8,0.5),1)
     if (choice == 1){
-      fig <-  octaFunc(p,q)
+      fig <-  octaFunc(p,q,w,h)
     }
     if (choice == 2){
-      fig <-  pentFunc(p,q)
+      fig <-  pentFunc(p,q,w,h)
     }
     if (choice == 3){
-      fig <-  squFunc(p,q)
-    } else{
-      
-      fig <-  hexFunc(p,q)
+      fig <-  squFunc(p,q,w,h)
+    } 
+    if (choice == 4){
+
+      fig <-  hexFunc(p,q,w,h)
     }
     
-    scramble <- sample(c(T,F),1,prob = c(0.9,0.1))
-    if (scramble) { fig <- fig[sample(nrow(fig)),] }
+    if (choice != 5){
+      scramble <- sample(c(T,F),1,prob = c(0.9,0.1))
+      if (scramble) { fig <- fig[sample(nrow(fig)),] }
 
-    polygon(fig[,1],fig[,2])
+      polygon(fig[,1],fig[,2])
+    }
+    else(
+      points(p,q,pch=1,cex=sample(seq(0.5,3,0.5),1))
+    )
   }}
 
+plot(0, 0,
+     xlim=c(0,15),
+     ylim=c(0,15),
+     col = "white", xlab = "", ylab = "", axes=F)
+
+for (i in 1:15){
+  for (j in 1:15){
+    print(c(i,j))
+  points(i,j,pch=i+j,cex=2)
+}}
+plot.new()
+points(0,0,pch=1,cex=3)
