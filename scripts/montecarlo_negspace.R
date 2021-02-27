da499d96987e392e27b5400e63a7fac6bd04cd3f
rodriguezmDNA <joelrome88@gmail.com>
2021-02-27 08:02:43 -0800

    white noise

---
+++ scripts/montecarlo_negspace.R
@@ -0,0 +1,37 @@
+library(grid)
+plot.new()
+
+
+center = 0 
+radius = 0.5
+
+plot(0, 0,
+     xlim=c(-2,2),
+     ylim=c(-2,2),
+     col = "white", xlab = "", ylab = "", axes=F)
+grid.circle(r = .103,x = 0.5205,y=0.511)
+points(center,center,pch=20)
+
+
+for (i in seq(1,30000/2)){
+  x <- runif(1,-1.5,1.5)
+  y <- runif(1,-1.5,1.5)
+  if ( abs(x-center) > l*2 | abs(y-center) > l){
+    #col <- sample(colors(),1)
+    #pch <- sample(1:12,1)
+    #points(x,y,pch='.',col=col)
+    points(x,y,pch='.')
+  }
+}
+
+
+for (i in seq(1,30000/2)){
+  x <- runif(1,-1.5,1.5)
+  y <- runif(1,-1.5,1.5)
+  if ( abs(x-center) > l | abs(y-center) > l*4){
+    #col <- sample(colors(),1)
+    #pch <- sample(1:12,1)
+    #points(x,y,pch='.',col=col)
+    points(x,y,pch='.')
+  }
+}
