c62bc7230462e998aa2c85f7e201b71c12fa52fc
rodriguezmDNA <joelrome88@gmail.com>
2021-03-13 09:27:28 -0800

    make into functions

--- scripts/parametric_figures_v2.R
+++ scripts/parametric_figures_v2.R
@@ -1,27 +1,46 @@
 ##http://www.sineofthetimes.org/the-art-of-parametric-equations-2/
 ### Messing around
 library(tidyverse)
-newX <- function(t,a,b){  (cos(a*t)) + ((cos(b*t))/2)    }
-newY <- function(t,a,b){  (sin(a*t)) + ((sin(b*t))/2)       }
 
+newX <- function(t,a,b){  (cos(a*t)) + ((cos(b*t))/2)     }
+newY <- function(t,a,b){  (sin(a*t)) + ((sin(b*t))/2)     }
 
-a=1
-b=5
-xa = c()
-ya = c()
-for (t in seq(1,1000,1)){
-  xa <- c(xa,newX(t,a,b))
-  ya <- c(ya,newY(t,a,b))
+makeParametricCurves <- function(a,b,maxReps=10,step=.01){
+  xa = c()
+  ya = c()
+  for (t in seq(1,maxReps,step)){
+    xa <- c(xa,newX(t,a,b))
+    ya <- c(ya,newY(t,a,b))
+  }
+  out2 <- data.frame( cbind(xa,ya))
+  return (out2)
 }
 
-pltLimit <- max(max(xa),max(ya)) 
-plot(0, 0,
+
+
+emptyCanvas <- function(pltLimit = 5 ){
+  plot(0, 0,
      xlim=c(-pltLimit,pltLimit),
      ylim=c(-pltLimit,pltLimit),
      col = "transparent", xlab = "", ylab = "", axes=F)
-out2 <- data.frame( cbind(xa,ya))
-lines(out2)
-head(out2)
+}
+emptyCanvas()
+lines(makeParametricCurves(0,10))
+
+emptyCanvas()
+
+for (i in seq(1,2)){
+  for (j in seq(1,8)){
+    lines(makeParametricCurves(i,j))
+    lines(makeParametricCurves(i,j)*-1)
+  }}
+
+emptyCanvas()
+lines(makeParametricCurves(1,1))
+lines(makeParametricCurves(1,2))
+lines(makeParametricCurves(1,2)* -1) 
+
+
 
 # ggplot(out2,aes(x=xa,y=ya)) +
 #   geom_path(color='lightgray') +
