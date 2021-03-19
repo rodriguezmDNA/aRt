8ef48194f1bd70a80b8397ea8a22316421a07a69
rodriguezmDNA <joelrome88@gmail.com>
2021-03-19 16:55:18 -0700

    change X and Y center

--- scripts/parametric_circle.R
+++ scripts/parametric_circle.R
@@ -2,26 +2,27 @@
 ### Messing around
 library(tidyverse)
 
-newX <- function(t,center){  (sin(t)+center)    }
-newY <- function(t,center){  (cos(t)+center)   }
+newX <- function(t,Xcenter){  (sin(t)+Xcenter)    }
+newY <- function(t,Ycenter){  (cos(t)+Ycenter)   }
 
 #makeParametricCurves <- function(a,b,maxReps=10,step=.01){
 
-makeCircle <- function(center=0,group='a'){
+makeCircle <- function(Xcenter=0,Ycenter=0,group='a'){
     xa = c()
     ya = c()
     for (t in seq(1,1000,1)){
-      xa <- c(xa,newX(t,center))
-      ya <- c(ya,newY(t,center))
+      xa <- c(xa,newX(t,Xcenter))
+      ya <- c(ya,newY(t,Ycenter))
     }
   out <- data.frame( cbind(xa,ya))
   out$group <- group
   return (out)
 }
 
-listOfCircles <- list(makeCircle(1,'a'),
-                      makeCircle(3,'b'),
-                      makeCircle(5,'c'))
+listOfCircles <- list(makeCircle(0,0,'a'),
+                      makeCircle(0,3,'b'),
+                      makeCircle(3,0,'c'),
+                      makeCircle(3,3,'d'))
 
 out2 = do.call('rbind',listOfCircles)
 tail(out2)
