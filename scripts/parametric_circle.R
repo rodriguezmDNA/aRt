49425eb4bbf30b0f2f742fb7688e7933dbcc5802
rodriguezmDNA <joelrome88@gmail.com>
2021-03-18 17:39:05 -0700

    function to make circles, change center

--- scripts/parametric_circle.R
+++ scripts/parametric_circle.R
@@ -2,35 +2,40 @@
 ### Messing around
 library(tidyverse)
 
-newX <- function(t,center){  (sin(t)+center)     }
+newX <- function(t,center){  (sin(t)+center)    }
 newY <- function(t,center){  (cos(t)+center)   }
 
 #makeParametricCurves <- function(a,b,maxReps=10,step=.01){
-xa = c()
-ya = c()
-for (t in seq(1,10,0.5)){
-  xa <- c(xa,newX(t,3))
-  ya <- c(ya,newY(t,3))
+
+makeCircle <- function(center=0,group='a'){
+    xa = c()
+    ya = c()
+    for (t in seq(1,1000,1)){
+      xa <- c(xa,newX(t,center))
+      ya <- c(ya,newY(t,center))
+    }
+  out <- data.frame( cbind(xa,ya))
+  out$group <- group
+  return (out)
 }
-out3 <- data.frame( cbind(xa,ya))
 
-a = cbind(out2,'a')
-b = cbind(out3,'b')
+listOfCircles <- list(makeCircle(1,'a'),
+                      makeCircle(3,'b'),
+                      makeCircle(5,'c'))
 
-data.frame(c(a,b))
-#return (out2)
-#}
+out2 = do.call('rbind',listOfCircles)
+tail(out2)
 
 ggplot(out2,aes(x=xa,y=ya)) +
-  geom_path(color='black') +
+  geom_path(aes(color=group)) +
   #theme_void()
-  # theme(
-  #   axis.ticks =   element_blank(),
-  #   axis.text =    element_blank(),
-  #   axis.title =   element_blank(),
-  #   panel.grid.major = element_blank(),
-  #   panel.grid.minor = element_blank(),
-  #   plot.background=element_rect(fill = "black"),
-  #   panel.background = element_rect(fill = 'black'))
+  theme(
+    axis.ticks =   element_blank(),
+    axis.text =    element_blank(),
+    axis.title =   element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    plot.background=element_rect(fill = "black"),
+    panel.background = element_rect(fill = 'black'))
   NULL
 
