2e5bb9e995cb5641e963a3d6879ede41d0b01c36
rodriguezmDNA <joelrome88@gmail.com>
2021-04-03 14:09:36 -0700

    Grid of parametric curves

--- scripts/quanta_ArtWithMath.R
+++ scripts/quanta_ArtWithMath.R
@@ -8,13 +8,24 @@ y <- function(t,a=6,b=2,c=14,d=3) { sin(t) + (sin(a*t)/b) + (cos(c*t) * 1/d) }
 
 
 
-drawParamCurves <- function(){
+
+getCurve <- function(){
   out <- rbind(cbind(x(0),y(0)))
-  a = sample(seq(0,11),1)
+  a = sample(seq(1,11),1)
+  b = sample(seq(1,11),1)
+  c = sample(seq(1,15),1)
+  d = sample(seq(1,11),1)
+  print(c(a,b,c,d))
   for (t in seq(0,sample(seq(9,15),1),.01)){
-    out <- rbind(out,cbind(x(t,a=a),y(t,a=a)))
+    out <- rbind(out,cbind(x(t,a=a,b=b,c=c,d=d),y(t,a=a,b=b,c=c,d=d)))
   }
   out <- out[-1,]
+  return (out)
+}
+
+drawParamCurves <- function(){
+  
+  out <- getCurve()
   print(paste0('a=',a))
   emptyCanvas(2)
   
@@ -23,4 +34,18 @@ drawParamCurves <- function(){
 }
 
 drawParamCurves()
+
+grpA = data.frame(getCurve())
+grpB = data.frame(getCurve() + 2)
+
+#### Grid of parametric curves
+emptyCanvas(-10)
+for (i in seq(-15,15,3)){
+  for (j in seq(-15,15,3)){
+    grpA = data.frame(getCurve())
+    grpA[,1] <- grpA[,1] + i
+    grpA[,2] <- grpA[,2] + j
+    lines(grpA[,1],grpA[,2])
+}}
+
 
