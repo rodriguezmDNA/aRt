19663fc6636fedea3ce991847eea597b11790860
rodriguezmDNA <joelrome88@gmail.com>
2021-04-02 18:17:00 -0700

    make into a function

--- scripts/quanta_ArtWithMath.R
+++ scripts/quanta_ArtWithMath.R
@@ -7,13 +7,20 @@ x <- function(t,a=6,b=2,c=14,d=3) { cos(t) + (cos(a*t)/b) + (sin(c*t) * 1/d) }
 y <- function(t,a=6,b=2,c=14,d=3) { sin(t) + (sin(a*t)/b) + (cos(c*t) * 1/d) }
 
 
-out <- rbind(cbind(x(0),y(0)))
-for (t in seq(0,10,.02)){
-  a=5
-  c=-15
-  out <- rbind(out,cbind(x(t,a=a,c=c),y(t,a=a,c=c)))
+
+drawParamCurves <- function(){
+  out <- rbind(cbind(x(0),y(0)))
+  a = sample(seq(0,11),1)
+  for (t in seq(0,sample(seq(9,15),1),.01)){
+    out <- rbind(out,cbind(x(t,a=a),y(t,a=a)))
+  }
+  out <- out[-1,]
+  print(paste0('a=',a))
+  emptyCanvas(2)
+  
+  lines(out[,1],out[,2])
+  
 }
 
-emptyCanvas(2)
-lines(out[,1],out[,2])
+drawParamCurves()
 
