c8cb9aafd16b1759483453252cfbe76303bcd8d7
rodriguezmDNA <joelrome88@gmail.com>
2021-04-06 13:45:10 -0700

    dot matrix pattern

--- scripts/210405_experimentalPatterns.R
+++ scripts/210405_experimentalPatterns.R
@@ -9,3 +9,15 @@ for (i in seq(-256,256,1)){
       points(i,j,pch='.')
     }
   }}
+
+
+### Dot matrix pattern
+dev.off()
+emptyCanvas(100)
+for (i in seq(-256,256,1)){
+  for (j in seq(-256,256,1)){
+    if (tan(i)/cos(j) <= sin(i)){
+      #print('True')
+      points(i,j,pch='.')
+    }
+  }}
