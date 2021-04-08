00f86f84226f842ffea25ffc2218a6b029057c3d
rodriguezmDNA <joelrome88@gmail.com>
2021-04-08 10:29:29 -0700

    symmetric dot matrix

--- scripts/210405_experimentalPatterns.R
+++ scripts/210405_experimentalPatterns.R
@@ -64,15 +64,36 @@ for (i in seq(-256,256,1)){
     }
   }}
 
+
+## Symmetric 
 emptyCanvas(100)
-for (i in seq(-256,256,1)){
-  for (j in seq(-256,256,1)){
+for (i in seq(0,256,1)){
+  for (j in seq(0,256,1)){
     if (i!=0 & j!=0){
-      if (abs(i)%%abs(j) == 0 | (abs(i)%%abs(j) == 2)) {
+      if (abs(i)%%abs(j) > 5 ){  #| (abs(i)%%abs(j) == 2)) {
         #print('True')
         points(i,j,pch='.')
+        points(-i,-j,pch='.')
+        points(-i,j,pch='.')
+        points(i,-j,pch='.')
       }
     }
   }}
 
+
+
+## Symmetric 
+emptyCanvas(100)
+for (i in seq(0,256,1)){
+  for (j in seq(0,256,1)){
+    if (i!=0 & j!=0){
+      if (abs(i)%%abs(j) > 5 ){  #| (abs(i)%%abs(j) == 2)) {
+        #print('True')
+        points(i,j,pch='.')
+        points(-i,-j,pch='.')
+        points(-i,j,pch='.')
+        points(i,-j,pch='.')
+      }
+    }
+  }}
 
