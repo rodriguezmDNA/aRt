367b6b447dc3951d5aec55ed80c86cbca147099d
rodriguezmDNA <joelrome88@gmail.com>
2021-04-07 20:10:42 -0700

    perspective

--- scripts/210405_experimentalPatterns.R
+++ scripts/210405_experimentalPatterns.R
@@ -21,3 +21,58 @@ for (i in seq(-256,256,1)){
       points(i,j,pch='.')
     }
   }}
+
+
+
+dev.off()
+emptyCanvas(100)
+for (i in seq(0,256,1)){
+  for (j in seq(0,256,1)){
+    if (tan(i)*cos(j) >= cos(i)){
+      #print('True')
+      points(i,j,pch='.')
+    }
+  }}
+
+for (i in seq(-256,0,1)){
+  for (j in seq(-256,0,1)){
+    if (tan(i)/cos(j) <= sin(i)){
+      #print('True')
+      points(i,j,pch='.')
+    }
+  }}
+
+
+for (i in seq(-256,0,1)){
+  for (j in seq(-256,0,1)){
+    if (tan(i)*cos(j) >= cos(i)){
+      #print('True')
+      points(i,j,pch='.')
+    }
+  }}
+
+
+#### Perspective
+emptyCanvas(100)
+for (i in seq(-256,256,1)){
+  for (j in seq(-256,256,1)){
+    if (i!=0 & j!=0){
+      if (abs(i)%%abs(j) == 0){
+        #print('True')
+        points(i,j,pch='.')
+      }
+    }
+  }}
+
+emptyCanvas(100)
+for (i in seq(-256,256,1)){
+  for (j in seq(-256,256,1)){
+    if (i!=0 & j!=0){
+      if (abs(i)%%abs(j) == 0 | (abs(i)%%abs(j) == 2)) {
+        #print('True')
+        points(i,j,pch='.')
+      }
+    }
+  }}
+
+
