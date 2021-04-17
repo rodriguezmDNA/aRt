5a2662fc029aa79cdb065626aceebdbb4fc267fb
rodriguezmDNA <joelrome88@gmail.com>
2021-04-17 12:54:32 -0700

    tear shaped

---
+++ sin-sin.R
@@ -0,0 +1,19 @@
+x <- function(t,a=2,b=2) { a*sin(t) -   sin(t*b)}
+y <- function(t,a=2,b=2) { a*cos(t) -   cos(t*b)}
+
+
+#### Tear shaped
+emptyCanvas(20)
+out <- rbind(cbind(x(0),y(0)))
+for (t in seq(0,10,.01)){
+  
+  xP <- x(t,4)
+  yP <- y(t,15)
+  out <- rbind(out,cbind(xP,yP))
+  points(xP,yP,pch='.')
+}
+out <- out[-1,]
+out <- data.frame(out)
+colnames(out) <- c('X','Y')
+ggplot(out,aes(x=X,y=Y)) +
+  geom_path() #+
