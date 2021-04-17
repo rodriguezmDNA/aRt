7382d9ad3f872b886c79bfa029fc101c8cafc672
rodriguezmDNA <joelrome88@gmail.com>
2021-04-17 12:54:57 -0700

    tear shaped new

--- sin-sin.R
+++ sin-sin.R
@@ -2,7 +2,8 @@ x <- function(t,a=2,b=2) { a*sin(t) -   sin(t*b)}
 y <- function(t,a=2,b=2) { a*cos(t) -   cos(t*b)}
 
 
-#### Tear shaped
+##### 20210417
+### tear shape
 emptyCanvas(20)
 out <- rbind(cbind(x(0),y(0)))
 for (t in seq(0,10,.01)){
