fd2ccd2629eaf45090f1e633bee86151262d4a43
rodriguezmDNA <joelrome88@gmail.com>
2021-04-18 10:07:03 -0700

    functionize

--- scripts/spirographs.R
+++ scripts/spirographs.R
@@ -5,9 +5,9 @@ valY <- function(t,a,b){ ( (a-b) * sin(t) ) + b*sin(t* ((a/b)-1))}
 
 x = c()
 y = c()
-a=65.5
-b=-30
-for (t in seq(2,300,2.5)){
+a=14
+b=2
+for (t in seq(1,100,1)){
   x <- c(x,valX(t,a,b))
   y <- c(y,valY(t,a,b))
 }
@@ -15,7 +15,7 @@ for (t in seq(2,300,2.5)){
 pltLimit <- 83
 plot(0, 0,
      xlim=c(-pltLimit,pltLimit),
-   ylim=c(-pltLimit,pltLimit),
+     ylim=c(-pltLimit,pltLimit),
      col = "transparent", xlab = "", ylab = "", axes=F)
 out <- data.frame( cbind(x,y))
 lines(out)
--- sin-sin.R
+++ sin-sin.R
@@ -1,6 +1,12 @@
 x <- function(t,a=2,b=2) { a*sin(t) -   sin(t*b)}
 y <- function(t,a=2,b=2) { a*cos(t) -   cos(t*b)}
 
+emptyCanvas <- function(pltLimit = 5 ){
+  plot(0, 0,
+       xlim=c(-pltLimit,pltLimit),
+       ylim=c(-pltLimit,pltLimit),
+       col = "transparent", xlab = "", ylab = "", axes=F)
+}
 
 ##### 20210417
 ### tear shape
@@ -18,3 +24,32 @@ out <- data.frame(out)
 colnames(out) <- c('X','Y')
 ggplot(out,aes(x=X,y=Y)) +
   geom_path() #+
+
+
+
+
+##### 20210418
+### Make into a function, generate different curves 
+sinsin <- function(a=2,b=2,newCanvas=FALSE,makePlot=FALSE){
+  out <- rbind(cbind(x(0),y(0)))
+  if (newCanvas) {emptyCanvas()}
+  for (t in seq(0,10,.01)){
+    xP <- x(t,a)
+    yP <- y(t,b)
+    out <- rbind(out,cbind(xP,yP))
+    if (makePlot) {lines(xP,yP,pch='.')}
+    
+  }
+  out <- out[-1,]
+  out <- data.frame(out)
+  colnames(out) <- c('X','Y')
+return(out)
+}
+
+
+emptyCanvas(20)
+for (i in seq(1,10,4)){
+  for (j in seq(1,10,2)){
+    emptyCanvas(20)
+    lines(sinsin(j,i))
+}}
