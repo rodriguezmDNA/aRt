716edc4754bd000fbccd08de8339b41962f77f5e
rodriguezmDNA <joelrome88@gmail.com>
2021-04-27 12:04:43 -0700

    tri-trigo

--- 20210426_Cos_Waves.R
+++ 20210426_Cos_Waves.R
@@ -8,3 +8,15 @@ for (i in seq(-20,20,.1)){
   for (j in seq(-10,10,.1)){
   points(i+j,cos(i)+j,pch='.')
 }}
+
+
+##### 20210427
+### tri-trigo
+emptyCanvas(10)
+for (i in seq(-10,10,1)){
+  for (j in seq(-10,10,.1)){
+    points(j,sin(j)+i,pch='.',col='blue')
+    points(j,cos(j)+i,pch='.',col='red')
+    points(j,tan(j)+i,pch='.',col='black')
+  }
+}
