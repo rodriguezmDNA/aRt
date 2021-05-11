db008b29d7fd3283f1c0dc151218abf10439b262
rodriguezmDNA <joelrome88@gmail.com>
2021-05-10 19:10:54 -0700

    pass functions to dotmatrix pattern 2

--- scripts/dotMatrixPatterns.R
+++ scripts/dotMatrixPatterns.R
@@ -1,9 +1,6 @@
 
 
-
-sincos <- function(i) {sin(i)/cos(j) <= tan(i)}
-cossin <- function(i) {cos(i)/sin(j) <= tan(i)}
-
+##### 20210509
 dotmatrix <- function(size=256,step=10,
                       printDot=FALSE,
                       ggprint=TRUE,returnValues=FALSE,
@@ -45,11 +42,51 @@ dotmatrix <- function(size=256,step=10,
   
 }
 
+png('~/Desktop/618studiodesign/20210509_cossin.png', pointsize=10, height=4000, width=3200, res=600)
+dotmatrix(size=20,step=.2)
+dev.off()
 
-dotmatrix(size=256,step=2)
-dotmatrix(size=256,step=2,operation = function(i) {cos(i)/sin(j) <= tan(i)})
 
+##### 20210510
+dotmatrix <- function(size=256,step=10,
+                      printDot=FALSE,
+                      ggprint=TRUE,returnValues=FALSE,
+                      operation = function(i) {cos(i)/sin(j) <= tan(i)}) {
+  out <- rbind(c(NA,NA))
+  if (printDot) {emptyCanvas()}
+  
+  for (i in seq(-size,size,step)){
+    for (j in seq(-size,size,step)){
+      if (operation(i)){
+        #print('True')
+        out <- rbind(out,rbind(c(i,j)))
+        if (printDot) {points(i,j,pch='.')}
+      }
+    }}
+  
+  outDF <- data.frame(out)
+  colnames(outDF) <- c('x','y')
+  
+  if (ggprint){
+    
+    ggout <- ggplot(outDF[-1,],aes(x=x,y=y)) +
+      geom_point(color='white',size=0.05) +
+      theme(
+        axis.ticks =   element_blank(),
+        axis.text =    element_blank(),
+        axis.title =   element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background=element_rect(fill = "black"),
+        panel.background = element_rect(fill = 'black'))
+    NULL
+    print(ggout)
+    
+  }
+  if (returnValues){
+    return (outDF)
+  }
+  
+}
 
-
-dotmatrix(size=200,step=2,condition = function(i){ tan(i)/sin(j) <= cos(i)} )
-
+dotmatrix2(size=20,step=.1)
