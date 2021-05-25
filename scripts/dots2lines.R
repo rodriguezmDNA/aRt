4fed8678b080f6894a2f50abdf32c2f2fc3aa179
rodriguezmDNA <joelrome88@gmail.com>
2021-05-24 19:20:49 -0700

    bla

--- scripts/dots2lines.R
+++ scripts/dots2lines.R
@@ -1,8 +1,8 @@
 getX <- function(t,a=6,b=2,c=14,d=3) { cos(t) + (cos(a*t)/b) + (sin(c*t) * 1/d) }
 getY <- function(t,a=6,b=2,c=14,d=3) { sin(t) + (sin(a*t)/b) + (cos(c*t) * 1/d) }
 
-xPts <- do.call(getX,list(seq(1,10,.5)))
-yPts <- do.call(getY,list(seq(1,10,.5)))
+xPts <- do.call(getX,list(seq(1,10,.15)))
+yPts <- do.call(getY,list(seq(1,10,.15)))
 
 #### Add markers to line
 emptyCanvas(4)
@@ -15,5 +15,26 @@ for (i in seq(1,length(xPts))){
     points(xPts[i],yPts[i],pch=24)
   }
     
+}
+
+####
+
+getX <- function(t,a=6,b=2,c=14,d=3) { cos(t)  }
+getY <- function(t,a=6,b=2,c=14,d=3) { sin(t) + (cos(a*t)/b) + (cos(c*t) * 1/d) }
+
+xPts <- do.call(getX,list(seq(1,10,.15)))
+yPts <- do.call(getY,list(seq(1,10,.15)))
+
+#### Add markers to line
+emptyCanvas(4)
+lines(cbind(xPts,yPts))
+for (i in seq(1,length(xPts))){
+  if (i%%3 == 0){
+    points(xPts[i],pch=22)
+  }
+  if (i%%4 == 0){
+    points(xPts[i],pch=24)
+  }
+  
 }
 
